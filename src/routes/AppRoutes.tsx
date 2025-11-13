2004bfbe81ce829944e11afb4be6ddf4c0f63048
Joakim Lönngren <joakim.lonngren@yh.nackademin.se>
2025-11-13 14:36:40 +0100

    Added MovieDetailsPage and made it responsive.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "../components/layout/Layout";
 import { HomePage } from "../features/movies/pages/homepage/HomePage";
 import { SearchPage } from "../features/movies/pages/searchpage/SearchPage";
-import MovieDetailPage from "../features/movies/pages/moviedetailpage/MovieDetailPage";
+import { MovieDetailPage } from "../features/movies/pages/moviedetailpage/MovieDetailPage";
 
 
 export const AppRoutes = () => (
@@ -10,7 +10,7 @@ export const AppRoutes = () => (
         <Routes>
             <Route path="/" element={<HomePage/>}/>
             <Route path="search" element={<SearchPage/>}/>
-            <Route path="/movie:id" element={<MovieDetailPage/>}/>
+            <Route path="/movie/:id" element={<MovieDetailPage/>}/>
         </Routes>
     </Layout>
 );
