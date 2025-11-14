dd66d4b24a5a40665ca247bfcbe5829c133f5903
Joakim Lönngren <joakim.lonngren@yh.nackademin.se>
2025-11-14 10:18:31 +0100

    Implemented mediadetailLayout in moviedetailpage.

--- src/features/movies/pages/moviedetailpage/MovieDetailPage.tsx
+++ src/features/movies/pages/moviedetailpage/MovieDetailPage.tsx
@@ -1,11 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useMovieDetails } from "../../hooks/useMovieDetails";
 import { useFavoritesStore } from "../../../../store/useFavoritesStore";
-import GlobalButton from "../../../../components/common/button/GlobalButton";
+import { MediaDetailLayout } from "../../../../components/media/mediadetaillayout/MediaDetailLayout";
 import styles from "./MovieDetailPage.module.scss";
-
-const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
-const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342";
 
 export const MovieDetailPage = () => {
     const { id } = useParams<{ id: string }>();
@@ -30,52 +27,16 @@ export const MovieDetailPage = () => {
     }
 
     return (
-        <article className={styles.container}>
-            {movie.backdropPath && (
-                <div className={styles.backdropWrapper}>
-                    <img
-                        src={`${BACKDROP_BASE_URL}${movie.backdropPath}`}
-                        alt={movie.title}
-                        className={styles.backdrop}
-                    />
-                </div>
-            )}
-
-            <div className={styles.content}>            
-                <div className={styles.details}>
-                    {movie.posterPath && (
-                        <img
-                            src={`${POSTER_BASE_URL}${movie.posterPath}`}
-                            alt={movie.title}
-                            className={styles.poster}
-                        />
-                    )}
-                    {/* <GlobalButton
-                        type="button"
-                        className={styles.favoriteButton}
-                        onClick={() => toggleFavorite(movie)}
-                        label={isFavorite ? "Remove favorite" : "Add favorite"}
-                    /> */}
-                    
-                </div>
-
-                <div className={styles.text}>
-                    <h1 className={styles.title}>{movie.title}</h1>
-
-                    <p className={styles.releaseDate}>{movie.releaseDate?.slice(0,4) ?? "Unknown year"}</p>
-                    <p className={styles.rating}>⭐ {movie.voteAverage.toFixed(1)} / 10</p>
-                    
-                    <p className={styles.overview}>{movie.overview}</p>
-
-                    <GlobalButton
-                        type="button"
-                        className={styles.favoriteButton}
-                        onClick={() => toggleFavorite(movie)}
-                        label={isFavorite ? "Remove favorite" : "Add favorite"}
-                    />
-                </div>
-            </div>
-        </article>
+        <MediaDetailLayout
+            title={movie.title}
+            releaseYear={movie.releaseDate}
+            rating={movie.voteAverage}
+            overview={movie.overview}
+            backdropPath={movie.backdropPath}
+            posterPath={movie.posterPath}
+            isFavorite={isFavorite}
+            onToggleFavorite={() => toggleFavorite(movie)}
+        />
     );
 };
 
